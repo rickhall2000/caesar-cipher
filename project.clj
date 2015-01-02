d7609153542b3eee5c07da6edddc3378da7ef0d7
Rick Hall <rickhall2000@hotmail.com>
2015-01-02 16:01:13 -0500

    add types

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/test.check "0.5.9"]])
+                 [org.clojure/test.check "0.5.9"]
+                 [org.clojure/core.typed "0.2.74"]]
+  :core.typed {:check [caesar-cipher.core]}
+  :plugins [[lein-typed "0.3.5"]])
--- src/caesar_cipher/core.clj
+++ src/caesar_cipher/core.clj
@@ -1,23 +1,30 @@
-(ns caesar-cipher.core)
+(ns caesar-cipher.core
+  (:require [clojure.core.typed :refer [ann] :as t]))
 
+(ann lower-case? [Number -> Boolean])
 (defn lower-case? [c]
   (and (>= (int c) (int \a))
        (<= (int c) (int \z))))
 
+(ann let-to-num [char -> Number])
 (defn let-to-num [l]
   (- (int l) (int \a)))
 
+(ann num-to-let [Number -> char])
 (defn num-to-let [l]
   (char (+ l (int \a))))
 
+(ann shift [Number Number -> Number])
 (defn shift [n i]
   (mod (+ i n) 26))
 
+(ann xlate-let [Number char -> char])
 (defn xlate-let [n let]
   (->> (let-to-num let)
        (shift n)
        (num-to-let)))
 
+(ann ^:no-check translate [Number String -> String])
 (defn translate [n phrase]
   (apply str
          (for [letter phrase]
